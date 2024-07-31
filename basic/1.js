1708ff2bcf33e6f26a39a22a731d307f4424e328
TheSniezek <maciek.wisniewski16@gmail.com>
2024-07-31 15:53:37 +0200

    basic 1 time task

--- basic/1.js
+++ basic/1.js
@@ -5,9 +5,21 @@
 // Current time is : 10 PM : 30 : 38
 
 const currentDate = new Date();
-const days = ['poniedziałek', 'wtorek', 'środa', 'czwartek', 'piątek', 'sobota', 'niedziela'];
+
+const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 const currentDay = currentDate.getDay(); // here to do something with currentDate
-const currentTime = currentDate.getTime(); // here to do something with currentDate
+
+const hours = currentDate.getHours();
+const minutes = currentDate.getMinutes();
+const seconds = currentDate.getSeconds();
+
+const ampm = hours >= 12 ? 'PM' : 'AM';
+const fixedMinutes = minutes < 10 ? '0' + minutes : minutes;
+const fixedSeconds = seconds < 10 ? '0' + seconds : seconds;
+
+const twelveClock = hours % 12 || 12;
+
+const currentTime = `${twelveClock} ${ampm} : ${fixedMinutes} : ${fixedSeconds}`; // here to do something with currentDate
 
 console.log(`Today is: ${days[currentDay - 1]}`);
 console.log(`Current time is: ${currentTime}`);
