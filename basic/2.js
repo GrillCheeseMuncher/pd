523c944d958e64e6bd0b5484f06c60893d6b03ae
TheSniezek <maciek.wisniewski16@gmail.com>
2024-07-31 15:59:22 +0200

    basic 2 time task

--- basic/2.js
+++ basic/2.js
@@ -4,6 +4,14 @@
 // mm-dd-yyyy, mm/dd/yyyy or dd-mm-yyyy, dd/mm/yyyy
 
 const currentDate = new Date();
-const formattedDate = undefined; // here to do something with currentDate
+
+const day = currentDate.getDay();
+const month = currentDate.getMonth();
+const year = currentDate.getFullYear();
+
+const fixedDay = day < 10 ? '0' + day : day;
+const fixedMonth = month < 10 ? '0' + month : month;
+
+const formattedDate = `${fixedDay}-${fixedMonth}-${year}`; // here to do something with currentDate
 
 console.log(formattedDate);
