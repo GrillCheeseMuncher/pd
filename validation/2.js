73832d21dcd4ee4e9ff63965192bd3c80e272256
Master of the Universe <r.michniuk@gmail.com>
2024-08-02 07:37:01 +0200

    set of tasks 2

---
+++ validation/2.js
@@ -0,0 +1,13 @@
+// Write a JavaScript function to validate whether a given value type is NaN or not
+
+// Test Data :
+// console.log(is_nan(NaN));
+// console.log(is_nan('bar'));
+// Expected Output :
+// true
+// false
+
+const is_nan = () => {};
+
+console.log(is_nan(NaN));
+console.log(is_nan('bar'));
